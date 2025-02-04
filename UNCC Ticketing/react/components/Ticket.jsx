d02bc4966487585610d1bbfe13154fb159d8fb50
Danielle Freeman <daniellefreeman2003@gmail.com>
2025-02-04 15:51:02 -0500

    Form

---
+++ UNCC Ticketing/react/components/Ticket.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+function Ticket(){
+    return(
+        <form>
+            <label> Enter First Name: 
+                <input type="text"/>
+            </label>
+            <label> Enter Last Name: 
+                <input type="text"/>
+            </label>
+
+            <label> Enter Building Name: 
+            
+            <select>
+                <option>Woodward</option>
+            </select>
+
+            <label>Location:
+
+
+            </label>
+
+            <label> Classroom:
+                <input type="checkbox" value="name"/>
+            </label>
+
+
+
+            
+
+            </label>
+        </form>
+    )
+}
+
+export default Ticket
