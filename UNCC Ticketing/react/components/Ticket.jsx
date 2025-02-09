7e8856ff49a5e9a1fc9fbef283cf1e0b8516e29e
YellowSegment <jgilbert10345@gmail.com>
2025-02-09 18:45:29 -0500

    More formatting
    Adjusted the formatting and input fields a little. I changed layout and added all buildings. Changed location type to a type=radio. Change text box to allow for resizing. Changed input for file to only allow images. Rounded out submit ticket button.
    
    Need to fix building name to allign the same way as the names on the first row being label and second row being the dropdown. Other than that, maybe some other formatting.

--- UNCC Ticketing/react/components/Ticket.jsx
+++ UNCC Ticketing/react/components/Ticket.jsx
@@ -10,9 +10,11 @@ function Ticket() {
 
                         <div className="first-last">
                             <label>First Name:
+                                <br/>
                                 <input type="text" required />
                             </label>
                             <label>Last Name:
+                                <br/>
                                 <input type="text" required />
                             </label>
                         </div>
@@ -21,59 +23,97 @@ function Ticket() {
                             <div className="building-dropdown">
                                 <label>Enter Building Name:</label>
                                 <select>
-                                    <option>Woodward</option>
+                                    <option>Choose one</option>
+                                    <option>Athletic Training & Academic Center</option>
+                                    <option>Atkins Library</option>
+                                    <option>Barnhardt Student Activity Center/Halton Arena</option>
+                                    <option>Bioinformatic Building</option>
+                                    <option>Barnard</option>
+                                    <option>Burson</option>
+                                    <option>Cameron Hall</option>
+                                    <option>College of Health & Human Services</option>
+                                    <option>Colvard</option>
+                                    <option>Bonnie E. Cone Center</option>
+                                    <option>Cypress Hall</option>
+                                    <option>Denny</option>
+                                    <option>Duke Centennial Hall</option>
+                                    <option>Energy Production & Infrastructure Center</option>
+                                    <option>Fretwell</option>
+                                    <option>Friday</option>
+                                    <option>Garinger</option>
+                                    <option>Grigg Hall</option>
+                                    <option>Belk Gymnasium</option>
+                                    <option>Hawthorn</option>
+                                    <option>Student Health Center</option>
+                                    <option>Johnson Band Center Building</option>
+                                    <option>Klien Hall</option>
+                                    <option>The building that shall not be named</option>
+                                    <option>Macy</option>
+                                    <option>McEniry</option>
+                                    <option>McMillan Greenhouse</option>
+                                    <option>Mebane Hall</option>
+                                    <option>Memorial Hall</option>
+                                    <option>Partnership Outreach Research to Accelerate Learning</option>
+                                    <option>University Recreation Center</option>
+                                    <option>Robinson Hall</option>
+                                    <option>Judy W. Rose Football Center</option>
+                                    <option>Rowe</option>
+                                    <option>Smith</option>
+                                    <option>Storrs</option>
+                                    <option>Student Union</option>
+                                    <option>University Recreation Center</option>
+                                    <option>Winningham</option>
+                                    <option>Witherspoon</option>
+                                    <option>Woodward Hall</option>
                                 </select>
-                            </div>
-
-                            <div className="location-select">
-                                <label className="location-label">Location:</label>
-                                <label> Classroom:
-                                    <input type="checkbox" value="name" />
-                                </label>
-
-                                <label>Bathroom:
-                                    <input type="checkbox" value="name"></input>
-                                </label>
-
-                                <label>Main Building:
-                                    <input type="checkbox" value="name"></input>
-                                </label>
-
-                                <label>Dorm:
-                                    <input type="checkbox" value="name"></input>
-                                </label>
-
-                                <label>Study Area:
-                                    <input type="checkbox" value="name"></input>
-                                </label>
-
-                                <label>Other:
-                                    <input type="text"></input>
-                                </label>
                             </div>
                         </div>
 
-                        <div className="add-photo">
-                            <label>Import photo of issue and location
-                                <input type="file" name="photo"></input>
-                            </label>
+                        <div class="location-type">
+                            <label className="location-label">Location:</label> 
+                            <div className="location-select">
+                                <label> Classroom:
+                                    <input type="radio" name="location" value="classroom" />
+                                </label>
+                                <label>Bathroom:
+                                    <input type="radio" name="location" value="bathroom"></input>
+                                </label>
+                                <label>Main Building:
+                                    <input type="radio" name="location" value="main-building"></input>
+                                </label>
+                                <label>Dorm:
+                                    <input type="radio" name="location" value="dorm"></input>
+                                </label>
+                                <label>Study Area:
+                                    <input type="radio" name="location" value="study-area"></input>
+                                </label>
+                                <label>Other:
+                                    <input type="radio" name="location" value="other"></input>
+                                </label>
+                            </div>
                         </div>
 
                         <div className="description">
-                        <label>Please Describe Issue and Location
-                            <input type="text"></input>
-                        </label>
+                            <label>Please Describe Issue and Location:
+                                <br></br>
+                                <textarea name="description"></textarea>
+                            </label>
+                        </div>
+
+
+                        <div className="add-photo">
+                            <label>Import photo of issue and location:
+                                <input type="file" name="photo" accept="image/*"></input>
+                            </label>
                         </div>
 
                         <div className="updates">
-                        <label>Do you want email updates on this ticket to your school email?
-                            <input type="checkbox"></input>
-                        </label>
+                            <label>Do you want email updates on this ticket to your school email?
+                                <input type="checkbox"></input>
+                            </label>
                         </div>
 
                         <button>Submit Ticket</button>
-                        
-
                     </form>
                 </div>
             </div>
