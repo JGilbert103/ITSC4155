3d02518fc83a2066a4dabbec2ada56f6df60645c
Danielle Freeman <daniellefreeman2003@gmail.com>
2025-02-12 00:36:51 -0500

    fixed mysql

--- UNCC Ticketing/react/database/database.js
+++ UNCC Ticketing/react/database/database.js
@@ -1,12 +1,23 @@
-import { createConnection } from 'mysql';
+import mysql from 'mysql2';
 
-var con = createConnection({
+var con = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "1234"
-});
+  password: "",
+  database: "tickets"
+}).promise()
+
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
+
+async function getTickets(){
+  const results = await con.query("SELECT * FROM requests")
+  return results
+}
+
+const tickets = await getTickets()
+console.log(tickets)
+
---
+++ UNCC Ticketing/react/database/tickets.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE tickets;
+USE tickets;
+
+CREATE TABLE requests(
+    id integer PRIMARY KEY AUTO_INCREMENT,
+    username TEXT NOT NULL,
+    issue TEXT NOT NULL,
+    building TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+INSERT INTO requests (username, issue, building, created)
+VALUES
+('John Smith', 'Bathroom sink is broken', 'Cato'),
+('Terry Johnson', 'Room outlet does not work', 'CHHS'),
+('Sarah Paulson', 'Printer out of ink', 'Fretwell')
+
+;
