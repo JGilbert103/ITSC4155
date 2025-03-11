2b85b846d7d31bcbad32b1a8e3864330bc67c41b
JGilbert103 <jgilbert10345@gmail.com>
2025-03-11 16:19:35 -0400

    Cleaned up some loose files
    Could not get about to change overflow. Will work on later

--- UNCC Ticketing/react/components/About.jsx
+++ UNCC Ticketing/react/components/About.jsx
@@ -4,7 +4,7 @@ import '../css/about.css'
 export const About = () => {
   return (
     <>
-    <section id="main">
+    <section id="main" className="disableOverflow">
             <figure className="image">
               <img className="img-aboutus" src="../images/copy.png"></img>
               <h1 className="us-title">About us</h1>
