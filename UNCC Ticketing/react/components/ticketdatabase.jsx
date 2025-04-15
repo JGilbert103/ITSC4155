ebea02235c8d8928d5c395505989838f5e205cd2
JGilbert103 <jgilbert10345@gmail.com>
2025-04-15 15:28:10 -0400

    Adding filtering to db
    also changed buildings txt to alphabeticallized

--- UNCC Ticketing/react/components/ticketdatabase.jsx
+++ UNCC Ticketing/react/components/ticketdatabase.jsx
@@ -6,58 +6,82 @@ import axios from 'axios';
 
 
 function TicketDatabase() {
-    const [buildingname, setBuildingname] = useState([]) 
-    
-    const [tickets, setTickets] = useState([])
+    const [buildingname, setBuildingname] = useState([]);
+    const [place, setPlace] = useState([]);
+    const [tickets, setTickets] = useState([]);
+
+    // Filter state
+    const [building, setBuilding] = useState('');
+    const [location, setLocation] = useState('All');
+    const [roomNumber, setRoomNumber] = useState('');
+    const [searchText, setSearchText] = useState('');
+
     useEffect(() => {
         axios.get('http://localhost:3001/getTickets')
-        .then(tickets => setTickets(tickets.data))
-        .catch(err => console.log(err))
-    }, [])
+            .then(res => setTickets(res.data))
+            .catch(err => console.log(err));
+    }, []);
 
-    useEffect(() =>{
-            fetch("/buildings.txt")
-                .then(response => response.text())
-                .then(text => setBuildingname(text.split("\n")))     
-        }, []);
-        const [place, setPlace] = useState([])
-        
-            useEffect(() =>{
-                fetch("/locations.txt")
-                    .then(response => response.text())
-                    .then(text => setPlace(text.split("\n")))
-            }, []);
+    useEffect(() => {
+        fetch("/buildings.txt")
+            .then(response => response.text())
+            .then(text => setBuildingname(text.split("\n")));
+    }, []);
 
-                const [firstname, setFirstName] = useState('')
-                const [lastname, setLastName] = useState('')
-                const [problem, setProblem] = useState('')
-                const [building, setBuilding] = useState('')
-                const [location, setLocation] = useState('All')
-                const [updates, setUpdates] = useState('')
-                const [image, setImage] = useState(null)
+    useEffect(() => {
+        fetch("/locations.txt")
+            .then(response => response.text())
+            .then(text => setPlace(text.split("\n")));
+    }, []);
 
+    const filteredTickets = tickets.filter(ticket => {
+        const fullName = `${ticket.firstname} ${ticket.lastname}`.toLowerCase();
+        const nameMatch = fullName.includes(searchText.toLowerCase());
+
+        const buildingMatch = building === '' || ticket.building === building;
+        const locationMatch = location === 'All' || ticket.location === location.replace(':', '');
+
+        const roomMatch =
+            (location === 'Classroom' || location === 'Dorm')
+                ? ticket.roomnumber?.toLowerCase().includes(roomNumber.toLowerCase())
+                : true;
+
+        return nameMatch && buildingMatch && locationMatch && roomMatch;
+    });
 
     return (
         <div className='fullPage'>
             <div className='leftPanel'>
                 <div className='filtersMain'>
                     <ul className='filters'>
-                        <li className='buttonObject'><a href='adminportal'><button className='backButton'>&lt;- Back</button></a></li>
+                        <li className='buttonObject'>
+                            <a href='adminportal'>
+                                <button className='backButton'>&lt;- Back</button>
+                            </a>
+                        </li>
+
                         <li className='searching'>
                             <div className='searchBox'>
-                                <input type="text" placeholder="Search By User..." />
+                                <input
+                                    type="text"
+                                    placeholder="Search By User..."
+                                    value={searchText}
+                                    onChange={(e) => setSearchText(e.target.value)}
+                                />
                             </div>
                         </li>
+
                         <li>
                             <div className='buildingName'>
-                                <select value={building} onChange={(e) => setBuilding(e.target.value)}> 
-                                    <option>Choose one</option>
-                                    {buildingname.map((name, i) =>(
+                                <select value={building} onChange={(e) => setBuilding(e.target.value)}>
+                                    <option value="">Select Building...</option>
+                                    {buildingname.map((name, i) => (
                                         <option key={i} value={name}>{name}</option>
                                     ))}
                                 </select>
                             </div>
                         </li>
+
                         <li>
                             <div className='Places'>
                                 <label>
@@ -70,23 +94,38 @@ function TicketDatabase() {
                                         onChange={(e) => setLocation(e.target.value)}
                                     />
                                 </label>
-                            {place.map((name, i) => (
-                                <label key={i}>
-                                    {name}
-                                    <input
-                                        type="radio"
-                                        value={name}
-                                        name="location"
-                                        checked={location === name}
-                                        onChange={(e) => setLocation(e.target.value)}
-                                    />
-                                </label>
+
+                                {place.map((name, i) => (
+                                    <label key={i}>
+                                        {name}
+                                        <input
+                                            type="radio"
+                                            value={name}
+                                            name="location"
+                                            checked={location === name}
+                                            onChange={(e) => setLocation(e.target.value)}
+                                        />
+                                    </label>
                                 ))}
+
+                                {(location === "Classroom" || location === "Dorm") && (
+                                    <div className="room-number">
+                                        <label>Room Number:
+                                            <br />
+                                            <input
+                                                type="text"
+                                                value={roomNumber}
+                                                onChange={(e) => setRoomNumber(e.target.value)}
+                                            />
+                                        </label>
+                                    </div>
+                                )}
                             </div>
                         </li>
                     </ul>
                 </div>
             </div>
+
             <div className='rightPanel'>
                 <div className='allTickets'>
                     <table>
@@ -96,53 +135,31 @@ function TicketDatabase() {
                                 <th>Name</th>
                                 <th>Building</th>
                                 <th>Location</th>
+                                <th>Room</th>
                                 <th>Action</th>
                             </tr>
                         </thead>
                         <tbody className='listedTickets'>
-                            <tr>
-                                <td>1</td>
-                                <td>Gilbert, Jake</td>
-                                <td>Fretwell</td>
-                                <td>Bathroom</td>
-                                <td><a href=''><button className='viewButton'>view</button></a></td>
-                            </tr>
-                            <tr>
-                                <td>2</td>
-                                <td>Shugarman, Tess</td>
-                                <td>Colvard</td>
-                                <td>Hallway</td>
-                                <td><a href=''><button className='viewButton'>view</button></a></td>
-                            </tr>
-                            <tr>
-                                <td>3</td>
-                                <td>Masangkay, Mico</td>
-                                <td>Denny</td>
-                                <td>Classroom</td>
-                                <td><a href=''><button className='viewButton'>view</button></a></td>
-                            </tr>
-                            <tr>
-                                <td>4</td>
-                                <td>Freeman, Danielle</td>
-                                <td>Prospector</td>
-                                <td>Bathroom</td>
-                                <td><a href=''><button className='viewButton'>view</button></a></td>
-                            </tr>
-
-                            {
-                            tickets.map(ticket => {
-                                return <tr>
-                                    <td>N/A</td>
-                                    <td>{ticket.lastname}, {ticket.firstname}</td> 
-                                    <td>{ticket.building}</td> 
-                                    <td>{ticket.location}</td> 
-                                    <td><a href=''><button className='viewButton'>view</button></a></td>
-                                  
-                                   
+                            {filteredTickets.length > 0 ? (
+                                filteredTickets.map((ticket, index) => (
+                                    <tr key={index}>
+                                        <td>N/A</td>
+                                        <td>{ticket.lastname}, {ticket.firstname}</td>
+                                        <td>{ticket.building}</td>
+                                        <td>{ticket.location}</td>
+                                        <td>{ticket.roomnumber || 'N/A'}</td>
+                                        <td>
+                                            <a href=''>
+                                                <button className='viewButton'>view</button>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))
+                            ) : (
+                                <tr>
+                                    <td colSpan="6" style={{ textAlign: 'center' }}>No tickets found.</td>
                                 </tr>
-                                        
-                                    })
-                                }
+                            )}
                         </tbody>
                     </table>
                 </div>
